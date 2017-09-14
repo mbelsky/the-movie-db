a1be15c173b5d4f930bc7ddc5b2406a39d2d35c8
Maxim Belsky <public.belsky@gmail.com>
2017-09-14 13:41:43 +0700

    Setup project

---
+++ the-movie-db/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  the-movie-db
+//
+//  Created by Maxim Belsky on 14/09/2017.
+//  Copyright © 2017 Maxim Belsky. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ the-movie-db/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  the-movie-db
+//
+//  Created by Maxim Belsky on 14/09/2017.
+//  Copyright © 2017 Maxim Belsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
