8be2d7aea81348218bbcfc0086d93be26542bc4f
Maxim Belsky <public.belsky@gmail.com>
2017-09-15 19:41:54 +0700

    Make Movies Equatable

--- the-movie-db/Movie.swift
+++ the-movie-db/Movie.swift
@@ -30,3 +30,9 @@ struct Movie: CustomStringConvertible {
         self.posterPath = posterPath
     }
 }
+
+extension Movie: Equatable {
+    static func ==(lhs: Movie, rhs: Movie) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
